a3013dcffbd003f60cf500075757e5f6e7fd3e16
Cameron Johnston <cameron@needle.com>
2012-06-08 12:13:32 -0600

    fixing more mongodb.agent_prefix inconsistency

--- recipes/mms-agent.rb
+++ recipes/mms-agent.rb
@@ -1,11 +1,14 @@
 include_recipe 'python'
 include_recipe 'runit'
 
+data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
+secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
+
 %w{ simplejson hmac hashlib pymongo }.each do |egg|
   python_pip egg
 end
 
-directory node[:mongodb][:agent_install_path] do
+directory node[:mongodb][:agent_prefix] do
   recursive true
 end
 
@@ -17,14 +20,14 @@ end
 
 remote_file "/tmp/10gen-mms-agent.tar.gz" do
   source "https://mms.10gen.com/settings/10gen-mms-agent.tar.gz"
-  not_if { ::File.exists?(::File.join(node[:mongodb][:agent_install_path],'agent.py')) }
+  not_if { ::File.exists?(::File.join(node[:mongodb][:agent_prefix],'agent.py')) }
   notifies :run, "bash[extract mms-agent]", :immediately
 end
 
-template ::File.join(node[:mongodb][:agent_install_path],'settings.py') do
+template ::File.join(node[:mongodb][:agent_prefix],'settings.py') do
   source 'mms-agent-settings.py.erb'
-  variables( :api_key => node[:mongodb][:agent_api_key],
-             :secret_key => node[:mongodb][:agent_secret_key] )
+  variables( :api_key => secrets['mongodb']['agent_api_key'],
+             :secret_key => secrets['mongodb']['agent_secret_key'] )
 end
 
 runit_service "mms-agent"
